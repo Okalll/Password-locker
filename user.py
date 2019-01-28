0b6565ed1cc9bfed1a9a35f235af27ac61e3c35e
Okall Vivian <vivieokall@gmail.com>
2019-01-28 16:11:51 +0300

    "Add confirmation attribute"

--- user.py
+++ user.py
@@ -1,14 +1,16 @@
 class User:
     """
-    class that generates new instances of user accounts
+    class that generates new instances of users
     """
     user_list = []  # empty Users list
 
     def save_user(self):
         '''
-        save_contact method saves user objects into user_list
+        save_user method saves user objects into user_list
         '''
         User.user_list.append(self)
+
+    
 
     def __init__(self, username, password, email):
         '''
@@ -18,3 +20,14 @@ class User:
         self.username = username
         self.password = password
         self.email = email
+
+    @classmethod
+    def confirm_user(cls, username, password, email):
+        '''
+        Method that checks if the name, password and email entered match entries in the users_list
+        '''
+        current_user = ''
+        for user in User.user_list:
+            if (user.username == username and user.password == password):
+                current_user = user.username
+        return current_user
