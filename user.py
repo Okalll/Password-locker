8c870e2b305010833e7f48749352f5f4cf5f0a85
Okall Vivian <vivieokall@gmail.com>
2019-01-28 14:31:28 +0300

    "Add attribute test"

--- user.py
+++ user.py
@@ -2,14 +2,19 @@ class User:
     """
     class that generates new instances of user accounts
     """
- 
-    user_list = [] #empty Users list
+    user_list = []  # empty Users list
 
-    def __init__(self,username,password,email):
+    def save_user(self):
+        '''
+        save_contact method saves user objects into user_list
+        '''
+        User.user_list.append(self)
+
+    def __init__(self, username, password, email):
         '''
         defining structure of the user object
         '''
 
-        self.username =username
+        self.username = username
         self.password = password
         self.email = email
