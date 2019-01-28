7cdc0d8c853edd623d3eaaaa83211218fa683725
Okall Vivian <vivieokall@gmail.com>
2019-01-28 12:18:42 +0300

    "Update files"

---
+++ userAccounts.py
@@ -0,0 +1,15 @@
+class UserAccounts:
+    """
+    class that generates new instances of users
+    """
+ 
+userAccount_list = [] #empty userAccount list
+
+def __init__(self, account_name, account_password):
+    '''
+    defining structure of the useraccount object
+    '''
+
+    self.account_name = account_name
+
+    self.account_password = account_password
